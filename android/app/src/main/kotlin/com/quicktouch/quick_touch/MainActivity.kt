f7e8c05e92bf2d50bd789b777bbde50d10f7047f
unknown <ranjank@beelabs.technology>
2024-06-13 22:40:43 +0530

    Making Mvvm architecture

---
+++ android/app/src/main/kotlin/com/quicktouch/quick_touch/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.quicktouch.quick_touch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/view/gif_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GifView extends StatefulWidget {
+  const GifView({super.key});
+
+  @override
+  State<GifView> createState() => _GifViewState();
+}
+
+class _GifViewState extends State<GifView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
